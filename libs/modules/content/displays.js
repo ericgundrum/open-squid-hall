be16acb7ea6ebf2d65809e365929f972279e15e4
void& <42633831+voidampersand@users.noreply.github.com>
2020-08-30 15:17:40 -0700

    Add the command module

---
+++ libs/modules/content/displays.js
@@ -0,0 +1,93 @@
+var displays = (function(){
+
+   return {
+      layouts: {
+         
+         "nz": {
+            "options": {
+            },
+            "objectPlacements": [
+               {
+                  "object": "wsfs.hugo-plinth",
+                  "data": [
+                     {
+                        "place-name": "retro-hugo",
+                        "options": {
+                           "placer": "PlinthPlacer",
+                           "moreInfoData": {
+                              "title": "Traffic Cone",
+                              "link-text": "Orange is new black in our road cone world",
+                              "link": "https://www.nzherald.co.nz/nz/news/article.cfm?c_id=1&objectid=11894187",
+                              "text": "Under construction!"
+                           }
+                        },
+                        "data": {
+                           "object": "trafficcone",
+                           "rotation": [
+                              0,
+                              0,
+                              0
+                           ],
+                           "scale": 1
+                        }
+                     }
+                  ]
+               },
+               {
+                  "object": "wsfs.retro-plinth",
+                  "data": [
+                     {
+                        "place-name": "retro-hugo",
+                        "options": {
+                           "placer": "PlinthPlacer",
+                           "moreInfoData": {
+                              "title": "Traffic Cone",
+                              "link-text": "Orange is new black in our road cone world",
+                              "link": "https://www.nzherald.co.nz/nz/news/article.cfm?c_id=1&objectid=11894187",
+                              "text": "Under construction!"
+                           }
+                        },
+                        "data": {
+                           "object": "trafficcone",
+                           "rotation": [
+                              0,
+                              0,
+                              0
+                           ],
+                           "scale": 1
+                        }
+                     }
+                  ]
+               },
+               {
+                  "object": "nz.sjv-plinth",
+                  "data": [
+                     {
+                        "place-name": "vogel",
+                        "options": {
+                           "placer": "PlinthPlacer",
+                           "moreInfoData": {
+                              "title": "Traffic Cone",
+                              "link-text": "Orange is new black in our road cone world",
+                              "link": "https://www.nzherald.co.nz/nz/news/article.cfm?c_id=1&objectid=11894187",
+                              "text": "Under construction!"
+                           }
+                        },
+                        "data": {
+                           "object": "trafficcone",
+                           "rotation": [
+                              0,
+                              0,
+                              0
+                           ],
+                           "scale": 1
+                        }
+                     }
+                  ]
+               },
+            ]
+         },
+      }
+   };
+})();
+SQUIDSPACE.addAutoloadModule(displays);
